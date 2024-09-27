aece81e645df9c8857bd1fc916d0df8f850c115e
Yavuz Selim <80224043+yssuzen@users.noreply.github.com>
2024-09-27 13:28:41 -0400

    Update SQLQueryforTableau.sql

--- SQLQueryforTableau.sql
+++ SQLQueryforTableau.sql
@@ -6,8 +6,8 @@ Queries used for Tableau Project
 
 
 
--- 1. 
 
+-- Global Numbers
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
@@ -15,6 +15,7 @@ where continent is not null
 --Group By date
 order by 1,2
 
+  -- Total Deaths per Continent
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
@@ -23,12 +24,14 @@ and location not in ('World', 'European Union', 'International')
 Group by location
 order by TotalDeathCount desc
 
+  -- Percentage of Population Infected per Country
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
+  --Percentage of Population Infected
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
